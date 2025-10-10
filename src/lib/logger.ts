ef37ec3d51f5e6430c4744640907c1c88e4aaad1
Nitin Nazeer <nitin.nazeer@gmail.com>
2025-10-09 18:25:41 -0700

    Improved logging DX, now `logger` works on client

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,11 +1,33 @@
-import pino from 'pino';
+import { browser } from '$app/environment';
+import pino, { type Logger } from 'pino';
 
-const logger = pino({
-	level: 'debug',
-	transport: {
-		target: 'pino-pretty',
-		options: { colorize: true }
-	}
-});
+// Define a universal logger interface
+interface UniversalLogger {
+	info: (...args: any[]) => void;
+	warn: (...args: any[]) => void;
+	error: (...args: any[]) => void;
+	debug: (...args: any[]) => void;
+}
+
+let logger: Logger | UniversalLogger;
+
+if (browser) {
+	// Browser: use console methods for DevTools compatibility
+	logger = {
+		info: (...args: any[]) => console.log(...args),
+		warn: (...args: any[]) => console.warn(...args),
+		error: (...args: any[]) => console.error(...args),
+		debug: (...args: any[]) => console.debug(...args)
+	};
+} else {
+	// Server: use pino with pino-pretty
+	logger = pino({
+		level: 'debug',
+		transport: {
+			target: 'pino-pretty',
+			options: { colorize: true }
+		}
+	});
+}
 
 export default logger;
